9a0962e3a5bf529c777286ea765cdaa9caf2476f
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2021-09-12 10:23:04 +0200

    remove no longer needed store variable

--- src/state.js
+++ src/state.js
@@ -3,5 +3,4 @@ import { writable } from 'svelte/store';
 export const booted = writable(false);
 export const accepted = writable(false);
 export const configured = writable(false);
-export const api = writable("localhost:8080");
 export const screen = writable("nav");

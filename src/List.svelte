cb88ffffe372b0bcf7e0c4b121d63073aadc2dda
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2021-10-17 21:27:40 +0200

    [#7] add objects screen

--- src/List.svelte
+++ src/List.svelte
@@ -1,13 +1,49 @@
 <script>
+	import { _ } from 'svelte-i18n';
+	
 	import Menu from './Menu.svelte';
+	import Preview from './Preview.svelte';
+
+	export let airrow;
+
+	const position = {
+		coords: {
+			latitude: 49.02138,
+			longitude: 12.10181
+		}
+	};
+	let markers = airrow.getPublicPoints(position);
 </script>
 
 <div class="screen">
-	no implementation yet
+	<div class="details">
+		<div class="details__heading">
+			<h1>Alle Kunstwerke</h1>
+		</div>
+
+		<div class="details__list">
+			{#await markers then data}
+				{#each data.points as m}
+				<Preview marker={m} />
+				{/each}
+			{/await}
+		</div>
+	</div>
 	<div class="menu">
 		<Menu />
 	</div>
 </div>
 
 <style>
+	.details {
+		grid-column-start: 1;
+		grid-column-end: -1;
+		grid-row-start: 1;
+		grid-row-end: -1;
+	}
+
+	.details__list {
+		max-height: calc(var(--vh, 1vh) * 80);
+		overflow: scroll;
+	}
 </style>
---
+++ src/Preview.svelte
@@ -0,0 +1,89 @@
+<script>
+	import { _ } from 'svelte-i18n';
+	import { screen } from './state.js';
+
+	import * as Util from './util.js';
+
+	function switchScreen(s) {
+		$screen = s;
+	}
+
+	export let marker;
+</script>
+
+<div class="details__element" on:click={() => switchScreen('details')}>
+	<div class="details__action">
+		<div class="details__favorite">
+			<img src="/assets/icons/preview/like.svg" alt="Markieren" />
+		</div>
+	</div>
+	<div class="details__teaser"></div>
+	<div class="details__description">
+		<div class="details__about">
+			<div class="details__title">
+				<h2>echo on survival</h2>
+			</div>
+			<div class="details__artist">
+				<p>Barbara Sophie Höcherl</p>
+			</div>
+		</div>
+		<div class="details__distance">{Util.humanDistance(marker.distance)} &gt;</div>
+	</div>
+</div>
+
+<style>
+	.details__element {
+		position: relative;
+
+		margin-left: 1em;
+		margin-right: 1em;
+		margin-bottom: 3em;
+	}
+
+	.details__teaser {
+		background-image: url('/assets/images/list/preview-default.jpg');
+		background-size: cover;
+		background-position: center;
+
+		height: 30vh;
+
+		border-top-left-radius: 1em;
+		border-top-right-radius: 1em;
+	}
+
+	.details__description {
+		display: grid;
+		grid-template-columns: 4fr 1fr;
+		grid-template-rows: 1fr;
+
+		padding-top: 1em;
+
+		background-color: white;
+
+		border-bottom-left-radius: 1em;
+		border-bottom-right-radius: 1em;
+	}
+
+	.details__about {
+		padding-left: 1em;
+	}
+
+	.details__title h2 {
+		margin-top: 0em;
+		margin-bottom: 0em;
+	}
+
+	.details__artist p {
+		margin-top: 0em;
+	}
+
+	.details__distance {
+		color: var(--font-color-normal);
+	}
+
+	.details__action {
+		position: absolute;
+		right: 0em;
+		margin: 0.5em;
+	}
+</style>

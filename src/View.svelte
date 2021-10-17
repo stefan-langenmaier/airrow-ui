258b8e8345aa6b1b9a3d1a1f1abeccd8610c2dc8
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2021-10-17 21:28:32 +0200

    [#6] add view screen

--- src/View.svelte
+++ src/View.svelte
@@ -1,5 +1,4 @@
 <script>
-	import { fade } from 'svelte/transition';
 	import { screen } from './state.js';
 	import * as Util from './util.js';
 	import '@google/model-viewer';
@@ -7,108 +6,77 @@
 	export let airrow;
 	let targetPromise = airrow.getTarget(airrow.targetRefCode);
 
-	let rating = "";
-	let isUploadingRating = false;
-	
-	function toggleRating(evt) {
-		const targetValue = evt.target.value;
-		if (targetValue == rating) {
-			rating = "";
-		}
-	}
-
-	function handleRating(refCode) {
-		if (isUploadingRating) {
-			return;
-		}
-		isUploadingRating = true;
-		const ratingUpload = airrow.rate(rating, refCode);
-		
-		ratingUpload.then(() => {
-			$screen = 'nav';
-		}).catch(() => {
-			isUploadingRating = false;
-		});
+	function switchScreen(s) {
+		$screen = s;
 	}
 </script>
 
-<div class="screen found" transition:fade={{ duration: 1000 }}>
-	{#if isUploadingRating}
-	<div class="poi" transition:fade={{ duration: 1000 }}>
-		<span class="loading">⏳</span>
-	</div>
-	{:else}
-		{#await targetPromise}
-		<div class="poi" transition:fade={{ duration: 1000 }}>
-			<span class="loading">⏳</span>
-		</div>
-		{:then target}
-		{#if rating === ""}
-		<div class="poi" transition:fade={{ duration: 1000 }}>
-			{#if airrow.hasTargetDownloadLink(target)}
-			<span>
-				{#if Util.isImage(target.mimeType)}
-				<!-- svelte-ignore a11y-missing-attribute -->
-				<img class="media" src="{airrow.getTargetDownloadLink(target)}"/>
-				{:else if Util.isAudio(target.mimeType)}
-				<!-- svelte-ignore a11y-media-has-caption -->
-				<audio class="media" controls src="{airrow.getTargetDownloadLink(target)}" />
-				{:else if Util.isVideo(target.mimeType)}
-				<!-- svelte-ignore a11y-media-has-caption -->
-				<video class="media" controls src="{airrow.getTargetDownloadLink(target)}" />
-				{:else if Util.isModel(target.mimeType)}
-				<model-viewer src="{airrow.getTargetDownloadLink(target)}" ar ar-modes="webxr scene-viewer quick-look" auto-rotate camera-controls></model-viewer>
-				{:else if Util.isLink(target.mimeType)}
-					{#await Util.get(airrow.getTargetDownloadLink(target))}
-					<span class="loading">⏳</span>
-					{:then link}
-						{#if Util.isYoutubeLink(link)}
-						<iframe title="inlined-video" src="{link}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-						{:else}
-						<a href="{link}" target="_blank">🔗↗️</a>
-						{/if}
-					{/await}
-				{:else}
-				<span class="target">🏁</span>
-				{/if}
-			</span>
+<div class="screen">
+	{#await targetPromise then target}
+	<div class="poi">
+		{#if airrow.hasTargetDownloadLink(target)}
+		<div class="media">
+			{#if Util.isImage(target.mimeType)}
+			<!-- svelte-ignore a11y-missing-attribute -->
+			<img src="{airrow.getTargetDownloadLink(target)}"/>
+			{:else if Util.isAudio(target.mimeType)}
+			<!-- svelte-ignore a11y-media-has-caption -->
+			<audio controls src="{airrow.getTargetDownloadLink(target)}" />
+			{:else if Util.isVideo(target.mimeType)}
+			<!-- svelte-ignore a11y-media-has-caption -->
+			<video controls src="{airrow.getTargetDownloadLink(target)}" />
+			{:else if Util.isModel(target.mimeType)}
+			<model-viewer src="{airrow.getTargetDownloadLink(target)}" ar ar-modes="webxr scene-viewer quick-look" auto-rotate camera-controls skybox-image="/assets/images/view/model-background-default.jpg" poster="/assets/images/view/preview-default.jpg"></model-viewer>
+			{:else if Util.isLink(target.mimeType)}
+				{#await Util.get(airrow.getTargetDownloadLink(target))}
+				<span class="loading">⏳</span>
+				{:then link}
+					{#if Util.isYoutubeLink(link)}
+					<iframe title="inlined-video" src="{link}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+					{:else}
+					<a href="{link}" target="_blank">🔗↗️</a>
+					{/if}
+				{/await}
 			{:else}
 			<span class="target">🏁</span>
 			{/if}
 		</div>
 		{:else}
-		<div class="poi" in:fade={{ duration: 1000 }}>
-			<span class="continue" on:click={() => handleRating(target.refCode)}>▶️</span>
-		</div>
+		<div class="media">🏁</div>
 		{/if}
-		<div class="rating">
-			<span>
-				<label>
-					<input type="radio" bind:group={rating} value="REPORT" on:click={toggleRating}>
-					<span>🚫</span>
-				</label>
-				
-				<label>
-					<input type="radio" bind:group={rating} value="DOWN" on:click={toggleRating}>
-					<span>👎</span>
-				</label>
-				
-				<label>
-					<input type="radio" bind:group={rating} value="UP" on:click={toggleRating}>
-					<span>👍</span>
-				</label>
-			</span>
+		<div class="action">
+			<div class="close" on:click="{() => switchScreen('map')}">
+				<img src="/assets/icons/map/close.svg" alt="Schliessen" />
+			</div>
 		</div>
-		{:catch error}
-		<div class="poi" transition:fade={{ duration: 1000 }}>
-			<span class="loading">⚠️</span>
+		<div class="description">
+			<div class="description__status">
+				<p>Kunstwerk erreicht</p>
+			</div>
+			<div class="description__title">
+				<h1>echo on survival</h1>
+			</div>
+			<hr />
+			<div class="description__action">
+				<div class="share">
+					<img src="/assets/icons/map/share.svg" alt="Teilen" />
+				</div>
+				<div class="like">
+					<img src="/assets/icons/map/like.svg" alt="Markieren" />
+				</div>
+			</div>
+			<div class="description__more">
+				<a href="/#" >
+					<img src="/assets/icons/map/info.svg" alt="Mehr" />
+					Mehr zum Kunstwerk
+				</a>
+			</div>
 		</div>
-		{/await}
-	{/if}
+	</div>
+	{/await}
 </div>
 
 <style>
-
 	.poi {
 		display: grid;
 		grid-template-columns: 1fr;
@@ -117,21 +85,10 @@
 		grid-column-start: 1;
 		grid-column-end: -1;
 		grid-row-start: 1;
-		grid-row-end: 2;
-	}
-
-	.rating {
-		display: grid;
-		grid-template-columns: 1fr;
-		grid-template-rows: 1fr;
-
-		grid-column-start: 2;
-		grid-column-end: 3;
-		grid-row-start: 2;
 		grid-row-end: -1;
 	}
 
-	.target {
+	.media {
 		display: grid;
 		grid-template-columns: 1fr;
 		grid-template-rows: 1fr;
@@ -139,36 +96,10 @@
 		grid-column-start: 1;
 		grid-column-end: -1;
 		grid-row-start: 1;
-		grid-row-end: 2;
-	}
-
-	.poi span {
-		margin: auto;
+		grid-row-end: -1;
 	}
 
 	span.target {
-		font-size: 40vh;
-	}
-
-	.rating span {
-		margin: auto;
-		font-size: 5vh;
-		padding: 1vh;
-	}
-
-	.rating input[type=radio] {
-  		display: none;
-	}
-
-	input[type="radio"]:checked + span {
-  		background-color: rgba(0,0,0,.5);
-	}
-
-	.rating label {
-		display: inline-block;
-	}
-
-	.continue {
 		font-size: 40vh;
 	}
 
@@ -176,30 +107,64 @@
 		font-size: 40vh;
 	}
 
-	.poi a {
-		font-size: 20vh;
+	.media * {
+		width: 100vw;
+		height: calc(var(--vh, 1vh) * 100);
 	}
 
-	.found {
-		background: radial-gradient(circle at 50%, lightgrey, orange 100%);;
+	.action {
+		position: absolute;
+		right: 1em;
+		display: flex;
 	}
 
-	.poi span img {
+	.close {
+		margin-top: 0.5em;
+	}
+
+	.description {
+		position: absolute;
+
+		bottom: 1em;
+		margin-left: 1em;
+		margin-right: 1em;
+
+		border-radius: 1em;
+		background-color: white;
+		box-shadow: 0 0 1em;
+
+		width: 100%;
 		max-width: 90vw;
-		max-height: 50vh;
 	}
 
-	.poi span iframe {
-		width: 90vw;
-		height: 50vh;
+	.description__action {
+		position: absolute;
+		right: 1em;
+		display: flex;
 	}
 
-	.poi span model-viewer {
-		width: 90vw;
-		height: 50vh;
+	.description__more img {
+		vertical-align: middle;
 	}
 
-	.poi span audio {
-		width: 90vw;
+	.description__more {
+		padding-left: 1em;
+		padding-bottom: 1em;
+	}
+
+	.description__status {
+		text-align: center;
+	}
+
+	.description__status p {
+		margin-bottom: 0em;
+	}
+
+	.description__title {
+		text-align: center;
+	}
+
+	.description__title h1 {
+		margin-top: 0em;
 	}
 </style>

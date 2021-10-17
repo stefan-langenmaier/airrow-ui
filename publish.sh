17685a90b2ccf2ab0e4c2d53df0757c10e93ff5f
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2021-10-17 21:34:33 +0200

    change publish script for branch

--- publish.sh
+++ publish.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-echo "build default"
-docker build . --build-arg CUSTOM_API_ENDPOINT="https://research.xn--rro-pla.de"  -t slangenmaier/airrow-ui
-docker push slangenmaier/airrow-ui
-
 echo "build donumenta"
 docker build . --build-arg CUSTOM_API_ENDPOINT="" -t slangenmaier/airrow-ui:donumenta-latest
 docker push slangenmaier/airrow-ui:donumenta-latest
